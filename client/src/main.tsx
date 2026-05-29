0dca4746677a64ef337cbbdbb06063b663ed5b7b
Matt <osth.mattias@gmail.com>
2026-05-29 13:39:32 +0900

    Analytics component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import { AuthProvider } from './app/authContext.tsx';
 import { router } from './app/router.tsx';
 import { queryClient } from './query/queryClient.ts';
@@ -22,5 +23,6 @@ createRoot(document.getElementById('root')!).render(
         </Suspense>
       </AuthProvider>
     </QueryClientProvider>
+    <Analytics />
   </StrictMode>,
 );
