ae7e1339b131d29c2c0894224b295036154e6b85
Matt <osth.mattias@gmail.com>
2026-05-29 22:32:37 +0900

    vercel speed insights

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { AuthProvider } from './app/authContext.tsx';
 import { router } from './app/router.tsx';
 import { queryClient } from './query/queryClient.ts';
@@ -24,5 +25,6 @@ createRoot(document.getElementById('root')!).render(
       </AuthProvider>
     </QueryClientProvider>
     <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 );
