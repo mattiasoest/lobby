9bea0994f6d424712d25dac1c7237507ce7d1e92
Matt <osth.mattias@gmail.com>
2026-05-29 20:41:59 +0900

    format+linting rules

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -7,7 +7,7 @@ import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'dist-ssr', 'coverage', 'node_modules']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
